5427d94bc623c7057552c989757e4f711351638c
ZeR0 <99215787+ZeR0vi@users.noreply.github.com>
2023-02-25 17:13:44 -0300

    Add files via upload

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import '../styles/main.scss'
+
+export function Header(){
+      return(
+
+      <header>
+      <div className="navigation">
+       <a href="#Skills">Skills</a>
+       <a href="#Projects">Projects</a>
+       <a href="#Contact">Contact</a>
+      </div>
+    </header>
+      )
+}
---
+++ components/Skills.tsx
@@ -0,0 +1,9 @@
+import '../styles/skills.scss'
+
+export function Skills(){
+      return(
+            <div className='skill'>
+                  <h1 className='title'>Skills</h1>
+            </div>
+      )
+}
