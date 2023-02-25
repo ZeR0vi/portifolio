c2885202215fa41ddb667cea1779ddf4abc65ff7
ZeR0 <99215787+ZeR0vi@users.noreply.github.com>
2023-02-25 17:20:01 -0300

    Delete components directory

--- components/Skills.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/skills.scss'
-
-export function Skills(){
-      return(
-            <div className='skill'>
-                  <h1 className='title'>Skills</h1>
-            </div>
-      )
-}
